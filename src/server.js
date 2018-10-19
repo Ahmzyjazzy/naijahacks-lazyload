ea67d82daba525d9958fc595b1ba22a29f5ebd53
Ahmzyjazzy <olanrewajuahmed095@yahoo.com>
2018-10-19 23:37:22 +0100

    update

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema');
+const schema = require('./data/schema');
+
+console.log(schema);
+
 const mongoose = require('mongoose');
 const cors = require('cors');
 
@@ -8,20 +11,26 @@ const app = express();
 
 app.use(cors());
 
-process.env.NODE_ENV = 'development';
+const __DEV_ = process.env.NODE_ENV === 'development';
 
-const url = (process.env.NODE_ENV == 'development') ? 'mongodb://localhost:27017/thinklet' : 'mongodb://ahmzyjazzy:support123@ds133353.mlab.com:33353/thinklet';
+const url = (__DEV_) ? 'mongodb://localhost:27017/thinklet' : 'mongodb://ahmzyjazzy:support123@ds133353.mlab.com:33353/thinklet';
 
 //connect to mlab database
-mongoose.connect(url,{useNewUrlParser: true})
+mongoose.connect(url,{useNewUrlParser: true}).catch((err) => {
+  console.log('Error on start: ' + err.stack);
+  process.exit(1);
+});
 mongoose.connection.once('open', ()=>{
     console.log('connected to database');
+})
+mongoose.connection.on('error', (err)=>{
+  console.log(err);
 })
 
 
 //setup graphql endpoint middleware
 app.use('/graphql', graphqlHTTP({
-   schema,
+   schema:{},
    graphiql: true
 }));
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,83 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+// module.exports = {
+//   devtool: 'cheap-module-source-map',
+//   module: {
+//       rules: [
+//           { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader'},
+//           {
+//             test: /\.css$/,
+//             use: [ 'style-loader', 'css-loader' ]
+//           }
+//       ]
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({template: 'index.html'})
+//   ]
+
+// };
+
+// const nodeExternals = require('webpack-node-externals')
+
+const common = {
   devtool: 'cheap-module-source-map',
   module: {
-      rules: [
-          { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader'},
-          {
-            test: /\.css$/,
-            use: [ 'style-loader', 'css-loader' ]
-          }
-      ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({template: 'index.html'})
-  ]
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  }
+}
 
-};
+module.exports = [
+  {
+    ...common,
+    entry: './src/client',
+    output: {
+      path: `${__dirname}/public`
+    }
+  },
+  {
+    ...common,
+    target: 'node',
+    entry: './src/server'
+  }
+]
+
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const path = require('path')
+
+// const common = {
+//   devtool: 'cheap-module-source-map',
+//   module: {
+//       rules: [
+//           { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader'},
+//           {
+//             test: /\.css$/,
+//             use: [ 'style-loader', 'css-loader' ]
+//           }
+//       ]
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({template: 'index.html'})
+//   ]
+// }
+
+// module.exports = [
+//   {
+//     ...common,
+//     entry: './src/client',
+//     output: {
+//       path: path.resolve(__dirname, 'dist'),
+//       filename: 'bundle',
+//     }
+//   }
+//   // {
+//   //   ...common,
+//   //   // target: 'node',
+//   //   entry: './src/server'
+//   // }
+// ]
