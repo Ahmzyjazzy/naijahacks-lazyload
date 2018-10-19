7dce96abced5b34857696f8f013ab1ac7079c443
Ahmzyjazzy <olanrewajuahmed095@yahoo.com>
2018-10-19 05:43:41 +0100

    feat: ejecting from create react app

---
+++ webpack.server.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  module: {
+      rules: [
+          { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
+      ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: 'index.html'})
+  ]
+
+};
