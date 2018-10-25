f75fad3df157a56617ef0baec545db41cfafeae4
Ahmzyjazzy <olanrewajuahmed095@yahoo.com>
2018-10-25 09:40:50 +0100

    core: refactor

--- src/pages/Signup/Main.js
+++ src/pages/Signup/Main.js
@@ -13,17 +13,21 @@ export default class extends React.Component {
     return (
       <main className="signupBg" style={css}>
           <div className="row formParent" >
+              <div className="col s12 m7 l7 hide-on-med-and-down">      
+                <div className="container">
+                  {/* <img src={instImage} className="img-responsive"/> */}
+                  <div className="centered" style={{fontSize:'2em'}}>
+                  In need of a skill and willing to learn?
+                  Let us connect you with an instructor that have the skill you desire.
+                  </div>
+                </div>
+              </div>
               <div className="col s12 m5 l5 formContainer">
                 <h5 style={{textAlign:'center'}}>Create an account</h5>
                 <p style={{textAlign: 'center', paddingBottom:'20px'}}>or <Link to="/login" style={{textDecoration: 'underline'}} className="black-text">sign in to your account</Link></p>
                 <RegisterForm />
               </div>
-              <div className="col s12 m7 l7 hide-on-med-and-down">      
-                <div className="container">
-                  {/* <img src={instImage} className="img-responsive"/> */}
-                  <div className="centered" style={{fontSize:'5em'}}>Connect with your favourite instructor </div>
-                </div>
-              </div>
+              
           </div>
       </main>
     )
